216d5e07d4769d476e304111c94946c39dec41c9
d.pishnenko <d.pishnenko@dunice.net>
2022-09-06 17:43:58 +0300

    Make conditional rendering with Loader and ShowAlert components

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,26 +1,64 @@
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ShowAlert from "../../components/Alert/Alert";
+import { makeStyles } from "@material-ui/core/styles";
+
 import Header from "../../components/Header/Header";
+import Loader from "../../components/Loader/Loader";
 import MainCard from "../../components/MainCard/MainCard";
-const testNews = [
-  { id: 1, title: "First news", content: "News content 1", tag: "first" },
-  { id: 2, title: "Second news", content: "News content 2", tag: "second" },
-  { id: 3, title: "Third news", content: "News content 3", tag: "third" },
-  { id: 4, title: "Fourth news", content: "News content 4", tag: "fourth" },
-];
+import { fetchNews } from "../../redux/actions/fetchNews";
+
+const useStyles = makeStyles({
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  wrapper: {
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+});
 
 const MainPage = () => {
+  const classes = useStyles();
+
+  const dispatch = useDispatch();
+  const fetchedNews = useSelector((state) => state.newsState.news);
+  const error = useSelector((state) => state.newsState.error);
+  const isLoading = useSelector((state) => state.newsState.isLoading);
+
+  useEffect(() => {
+    dispatch(fetchNews());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <Loader />
+      </>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className={classes.container}>
+        <ShowAlert value={"error"} message={error.message} />
+      </div>
+    );
+  }
   return (
     <>
       <Header />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-around",
-          alignItems: "center",
-          flexWrap: "wrap",
-        }}
-      >
-        <MainCard news={testNews} />
+      <div className={classes.wrapper}>
+        {fetchedNews.length &&
+          fetchedNews.map((news) => (
+            <MainCard title={news.title} content={news.content} key={news.id} />
+          ))}
       </div>
     </>
   );
