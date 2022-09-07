71188127015c086ac320d0d08a95b259e2c4b76d
d.pishnenko <d.pishnenko@dunice.net>
2022-09-07 15:34:49 +0300

    Move Header from component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 
+import Header from "./components/Header/Header";
 import { store } from "./redux/store";
 import "./App.css";
 import MainPage from "./pages/MainPage/MainPage";
@@ -9,6 +10,7 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <Header />
         <MainPage />
       </div>
     </Provider>
