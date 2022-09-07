8fcfbbb392dd29f3cbb880571091b6e765bcc32d
d.pishnenko <d.pishnenko@dunice.net>
2022-09-07 15:50:25 +0300

    Add hoc memo

--- src/components/MainCard/MainCard.jsx
+++ src/components/MainCard/MainCard.jsx
@@ -1,40 +1,18 @@
 import React from "react";
+import { memo } from "react";
 import PropTypes from "prop-types";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
-  container: {
-    display: "flex",
-    justifyContent: "space-around",
-    alignItems: "center",
-    flexWrap: "wrap",
-  },
-  root: {
-    width: 350,
-    height: 250,
-    marginTop: "10vh",
-    margin: "auto",
-    display: "flex",
-    justifyContent: "space-around",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 20,
-  },
-  content: {
-    fontSize: 16,
-  },
-});
+import { useStyles } from "./style";
 
 const MainCard = ({ title, content }) => {
   const classes = useStyles();
   return (
     <>
       <Card className={classes.root}>
-        <CardContent>
+        <CardContent className={classes.inner}>
           <Typography className={classes.title} variant="h5" component="h2">
             {title}
           </Typography>
@@ -52,4 +30,4 @@ MainCard.propTypes = {
   content: PropTypes.string,
 };
 
-export default MainCard;
+export default memo(MainCard);
