6ce23caea9fb1782f00f9beff32fc1d1b331911c
d.pishnenko <d.pishnenko@dunice.net>
2022-09-07 15:50:44 +0300

    Add hoc memo

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,67 +1,56 @@
 import React from "react";
+import { memo } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import ShowAlert from "../../components/Alert/Alert";
-import { makeStyles } from "@material-ui/core/styles";
+import ShowAlert from "../../components/Alert/ShowAlert";
 
-import Header from "../../components/Header/Header";
 import Loader from "../../components/Loader/Loader";
 import MainCard from "../../components/MainCard/MainCard";
-import { fetchNews } from "../../redux/actions/fetchNews";
+import { fetchNews } from "../../redux/actions/news";
 
-const useStyles = makeStyles({
-  container: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  wrapper: {
-    display: "flex",
-    justifyContent: "space-around",
-    alignItems: "center",
-    flexWrap: "wrap",
-  },
-});
+import { useStyles } from "./style";
 
 const MainPage = () => {
   const classes = useStyles();
 
   const dispatch = useDispatch();
-  const fetchedNews = useSelector((state) => state.newsState.news);
-  const error = useSelector((state) => state.newsState.error);
-  const isLoading = useSelector((state) => state.newsState.isLoading);
+  const {
+    news: fetchedNews,
+    error,
+    isLoading,
+  } = useSelector((state) => state.newsReducer);
 
   useEffect(() => {
     dispatch(fetchNews());
   }, []);
 
   if (isLoading) {
-    return (
-      <>
-        <Loader />
-      </>
-    );
+    return <Loader />;
   }
 
   if (error) {
     return (
       <div className={classes.container}>
-        <ShowAlert value={"error"} message={error.message} />
+        <ShowAlert severity="error" message="Error" />
       </div>
     );
   }
   return (
     <>
-      <Header />
       <div className={classes.wrapper}>
-        {fetchedNews.length &&
+        {fetchedNews.length ? (
+          fetchedNews.length &&
           fetchedNews.map((news) => (
             <MainCard title={news.title} content={news.content} key={news.id} />
-          ))}
+          ))
+        ) : (
+          <div className={classes.container}>
+            <ShowAlert severity="warning" message="There is no news yet" />
+          </div>
+        )}
       </div>
     </>
   );
 };
 
-export default MainPage;
+export default memo(MainPage);
