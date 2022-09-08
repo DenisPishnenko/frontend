dcd81b344eda7c2ddfb42b32e0de4e4502c7887f
d.pishnenko <d.pishnenko@dunice.net>
2022-09-08 17:38:22 +0300

    Add login and register onclick handlers to buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,13 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import { useDispatch } from "react-redux";
 
 import { useStyles } from "./style";
+import { openModal } from "../../redux/actions/auth";
 
 const Header = () => {
+  const dispatch = useDispatch();
   const classes = useStyles();
   return (
     <div className={classes.root}>
@@ -16,7 +19,12 @@ const Header = () => {
           <Typography className={classes.title} variant="h6">
             News App
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" onClick={() => dispatch(openModal())}>
+            Login
+          </Button>
+          <Button color="inherit" onClick={() => dispatch(openModal())}>
+            Register
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
