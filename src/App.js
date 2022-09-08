615496aa1e0777f009e5a0895a170781e5d14043
d.pishnenko <d.pishnenko@dunice.net>
2022-09-08 17:36:10 +0300

    Add ShowModal component into

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Header from "./components/Header/Header";
 import MainPage from "./pages/MainPage/MainPage";
+import ShowModal from "./components/ShowModal/ShowModal";
 
 import "./App.css";
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <MainPage />
+      <ShowModal />
     </div>
   );
 }
