b56d79348387f27454156ae2f95e76de49d5e6f5
d.pishnenko <d.pishnenko@dunice.net>
2022-09-09 17:57:02 +0300

    Make authorization actions

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,4 +1,10 @@
-import { IS_AUTH_MODAL_OPEN, TOOGLE_MODAL } from "../../constants";
+import {
+  IS_AUTH_MODAL_OPEN,
+  TOOGLE_MODAL,
+  USER_AUTH_REQUESTED,
+  USER_REGISTER_FAILED,
+  USER_REGISTER_SUCCESSED,
+} from "../../constants";
 
 export const openModal = () => ({
   type: IS_AUTH_MODAL_OPEN,
@@ -6,4 +12,19 @@ export const openModal = () => ({
 
 export const toogleModal = () => ({
   type: TOOGLE_MODAL,
+});
+
+export const register = (payload) => ({
+  type: USER_AUTH_REQUESTED,
+  payload,
+});
+
+export const registerSuccessed = (payload) => ({
+  type: USER_REGISTER_SUCCESSED,
+  payload,
+});
+
+export const registerFailed = (error) => ({
+  type: USER_REGISTER_FAILED,
+  payload: error,
 });
