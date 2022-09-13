73464ac54592f54f264991b3b71c0c740f33af40
d.pishnenko <d.pishnenko@dunice.net>
2022-09-13 16:00:33 +0300

    Add constants

--- src/constants.js
+++ src/constants.js
@@ -5,22 +5,22 @@ export const NEWS_FETCHED_FAILED = "NEWS_FETCHED_FAILED";
 export const NEWS_FETCHED_REQUESTED = "NEWS_FETCH_REQUESTED";
 
 export const NEWS_WARNING = "There is no news yet";
-export const IS_AUTH_MODAL_OPEN = "IS_AUTH_MODAL_OPEN";
-export const TOOGLE_MODAL = "TOOGLE_MODAL";
+export const OPEN_MODAL = "IS_AUTH_MODAL_OPEN";
+export const CLOSE_MODAL = "TOOGLE_MODAL";
 
 export const USER_AUTH_REQUESTED = "USER_REGISTER_REQUESTED";
 export const USER_REGISTER_SUCCESSED = "USER_REGISTER_SUCCESSED";
 export const USER_REGISTER_FAILED = "USER_REGISTER_FAILED";
 
+export const USER_LOGIN_REQUESTED = "USER_LOGIN_REQUESTED";
 export const USER_LOGIN_SUCCESSED = "USER_LOGIN_SUCCESED";
 export const USER_LOGIN_FAILED = "USER_LOGIN_FAILED";
 
-export const USER_LOGOUT_SUCCESSED = "USER_LOGOUT_SUCCESSED";
-export const USER_LOGOUT_FAILED = "USER_LOGOUT_FAILED";
+export const USER_LOGOUT = "USER_LOGOUT";
 
 export const validateUser = () => {
   return Yup.object().shape({
-    name: Yup.string().required("Please enter your name"),
+    name: Yup.string(),
     email: Yup.string()
       .email("Invalid email")
       .required("Please enter the email"),
@@ -30,6 +30,11 @@ export const validateUser = () => {
 
 export const FORM_FIELDS = [
   { label: "Name", type: "text", name: "name" },
+  { label: "Email", type: "text", name: "email" },
+  { label: "Password", type: "text", name: "password" },
+];
+
+export const LOGGED_FIELDS = [
   { label: "Email", type: "text", name: "email" },
   { label: "Password", type: "text", name: "password" },
 ];
