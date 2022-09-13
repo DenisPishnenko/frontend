52531a4a22ee6019ac267b7c6a1aba6cb7072578
d.pishnenko <d.pishnenko@dunice.net>
2022-09-13 15:56:44 +0300

    Add signout function

--- src/api/registerApi.js
+++ src/api/registerApi.js
@@ -1,6 +1,6 @@
-import { api } from ".";
+import api from ".";
 
-export const registerUser = async (values) => {
-  const { data } = await api.post("/users", { user: values });
+export const signOutUser = async () => {
+  const { data } = await api.delete("/users/sign_out");
   return data;
 };
