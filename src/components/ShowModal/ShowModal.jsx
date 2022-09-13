425c0e8367e77c0e391b2af526b80495a62c52ce
d.pishnenko <d.pishnenko@dunice.net>
2022-09-13 15:58:00 +0300

    Change props and events

--- src/components/ShowModal/ShowModal.jsx
+++ src/components/ShowModal/ShowModal.jsx
@@ -3,28 +3,25 @@ import { useDispatch, useSelector } from "react-redux";
 
 import Modal from "@material-ui/core/Modal";
 
-import { toogleModal } from "../../redux/actions/auth";
+import { closeModal } from "../../redux/actions/auth";
 import AuthForm from "../AuthForm/AuthForm";
 
 import { useStyles } from "./style";
 
 const ShowModal = () => {
   const classes = useStyles();
-  const open = useSelector((state) => state.auth.open);
-  const user = useSelector((state) => state.auth.user);
-  console.log(user);
-  console.log(open);
+  const isOpenModal = useSelector((state) => state.auth.isOpenModal);
   const dispath = useDispatch();
   return (
     <Modal
-      open={open}
+      open={isOpenModal}
       className={classes.modal}
-      onClose={() => dispath(toogleModal())}
+      onClose={() => dispath(closeModal())}
       aria-labelledby="simple-modal-title"
       aria-describedby="simple-modal-description"
     >
       <div className={classes.paper}>
-        <h4 className={classes.title}>Modal window</h4>
+        <h4 className={classes.title}>Modal Authorization</h4>
         <AuthForm />
       </div>
     </Modal>
