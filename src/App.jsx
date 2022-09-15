82d99c3f561ab6dc4eee7ccfdb402f0e469447a5
d.pishnenko <d.pishnenko@dunice.net>
2022-09-15 17:38:32 +0300

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 import MainPage from './pages/MainPage/MainPage';
 import ShowModal from './components/ShowModal/ShowModal';
+import UserPage from './pages/UserPage/UserPage';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <MainPage />
-      <ShowModal />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/user/:id" element={<UserPage />} />
+        </Routes>
+        <ShowModal />
+      </BrowserRouter>
     </div>
   );
 }
