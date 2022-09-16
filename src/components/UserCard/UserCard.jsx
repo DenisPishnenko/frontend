d7a4e856c377c33e9f2289b6b549554322ad6e10
d.pishnenko <d.pishnenko@dunice.net>
2022-09-16 11:27:56 +0300

    Add card content to UserCard

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,40 +1,62 @@
 import React, { memo } from 'react';
-import { string, number } from 'prop-types';
+import { useSelector } from 'react-redux';
+import { shape } from 'prop-types';
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 import useStyles from './style';
+import image from '../../assets/user.png';
 
-function UserCard({ id, name, email }) {
+function UserCard({ user }) {
   const classes = useStyles();
+
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   return (
     <Card className={classes.root}>
       <CardContent className={classes.inner}>
-        <Typography className={classes.title} variant="h5" component="h2">
-          {name}
-        </Typography>
-        <Typography className={classes.content} variant="body2" component="p">
-          {email}
-        </Typography>
-        <Typography className={classes.content} variant="body2" component="p">
-          {id}
-        </Typography>
+        <div className={classes.wrapper}>
+          <img
+            src={image}
+            alt="user"
+          />
+          <div className={classes.inner}>
+            <Typography className={classes.title} variant="h5" component="h2">
+              Author:
+              {user.name}
+            </Typography>
+            <Typography className={classes.content} variant="body2" component="p">
+              Email:
+              {user.email}
+            </Typography>
+            <Typography className={classes.content} variant="body2" component="p">
+              Author ID:
+              {user.id}
+            </Typography>
+          </div>
+        </div>
+        {isLoggedIn && (
+        <div className={classes.buttons}>
+          <Button variant="outlined" color="primary">
+            CREATE NEWS
+          </Button>
+          <Button variant="outlined" color="primary">
+            EDIT PROFILE
+          </Button>
+        </div>
+        )}
       </CardContent>
     </Card>
   );
 }
 
 UserCard.propTypes = {
-  name: string,
-  email: string,
-  id: number,
+  user: shape(),
 };
 UserCard.defaultProps = {
-  name: '',
-  email: '',
-  id: null,
+  user: null,
 };
 
 export default memo(UserCard);
