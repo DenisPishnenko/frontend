610f98ab515212bdf55740698eb8f543af007a50
d.pishnenko <d.pishnenko@dunice.net>
2022-09-19 17:08:11 +0300

    Add conditional rendering

--- src/components/ShowModal/ShowModal.jsx
+++ src/components/ShowModal/ShowModal.jsx
@@ -5,6 +5,7 @@ import Modal from '@material-ui/core/Modal';
 
 import { closeModal } from '../../redux/actions/auth';
 import AuthForm from '../AuthForm/AuthForm';
+import EditForm from '../EditForm/EditForm';
 
 import useStyles from './style';
 
@@ -12,7 +13,9 @@ function ShowModal() {
   const classes = useStyles();
   const isOpenModal = useSelector((state) => state.auth.isOpenModal);
   const error = useSelector((state) => state.auth.error);
+  const editingError = useSelector((state) => state.user.error);
   const modalType = useSelector((state) => state.auth.modalType);
+  const editModalType = useSelector((state) => state.auth.editModalType);
   const isAuth = modalType === 'SIGN IN';
   const dispatch = useDispatch();
   return (
@@ -24,11 +27,21 @@ function ShowModal() {
       aria-describedby="simple-modal-description"
     >
       <div className={classes.paper}>
-        <h4 className={classes.title}>
-          {isAuth ? 'Authorization' : 'Registration'}
-        </h4>
-        {error && <h3 className={classes.error}>{error}</h3>}
-        <AuthForm />
+        {editModalType ? (
+          <div>
+            <h4 className={classes.title}>Editing</h4>
+            {editingError && (<h3 className={classes.error}>{editingError}</h3>)}
+            <EditForm />
+          </div>
+        ) : (
+          <div>
+            <h4 className={classes.title}>
+              {isAuth ? 'Authorization' : 'Registration'}
+            </h4>
+            {error && (<h3 className={classes.error}>{error}</h3>)}
+            <AuthForm />
+          </div>
+        )}
       </div>
     </Modal>
   );
