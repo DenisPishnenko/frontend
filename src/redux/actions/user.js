492da99d3a37325acb83314c816ed23eb5c24e0d
d.pishnenko <d.pishnenko@dunice.net>
2022-09-19 17:10:30 +0300

    Add actions for user editing

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,4 +1,11 @@
-import { USER_REQUESTED, USER_REQUESTED_FAILED, USER_REQUESTED_SUCCESSED } from '../../constants';
+import {
+  USER_REQUESTED, USER_REQUESTED_FAILED,
+  USER_REQUESTED_SUCCESSED,
+  USER_EDIT_REQUESTED,
+  USER_EDIT_REQUESTED_SUCCESSED,
+  USER_EDIT_REQUESTED_FAILED,
+  OPEN_EDIT_MODAL,
+} from '../../constants';
 
 export const fetchUser = (payload) => ({
   type: USER_REQUESTED,
@@ -13,4 +20,23 @@ export const fetchUserSuccessed = (payload) => ({
 export const fetchUserFailed = (payload) => ({
   type: USER_REQUESTED_FAILED,
   payload,
+});
+
+export const fetchUserEdited = (payload) => ({
+  type: USER_EDIT_REQUESTED,
+  payload,
+});
+
+export const fetchUserEditedSuccessed = (payload) => ({
+  type: USER_EDIT_REQUESTED_SUCCESSED,
+  payload,
+});
+
+export const fetchUserEditedFailed = (payload) => ({
+  type: USER_EDIT_REQUESTED_FAILED,
+  payload,
+});
+
+export const openEditModal = () => ({
+  type: OPEN_EDIT_MODAL,
 });
