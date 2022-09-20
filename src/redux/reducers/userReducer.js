93e4f37cbb3aeec62a5f9bd7ef7678739d6533da
d.pishnenko <d.pishnenko@dunice.net>
2022-09-20 17:09:05 +0300

    Add cases for checking and editing user

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,30 +1,55 @@
 import {
+  USER_EDIT_REQUESTED,
+  USER_EDIT_REQUESTED_FAILED,
+  USER_EDIT_REQUESTED_SUCCESSED,
   USER_REQUESTED, USER_REQUESTED_FAILED,
   USER_REQUESTED_SUCCESSED,
+  OPEN_EDIT_MODAL,
+  CLOSE_EDIT_MODAL,
+  CHECK_USER_REQUESTED,
+  CHECK_USER_REQUESTED_FAILED,
 } from '../../constants';
 
 const initialState = {
   user: null,
   error: null,
   isLoading: false,
+  isOpenModal: false,
 };
 
 const userReducer = (state = initialState, action = {}) => {
   switch (action.type) {
+    case OPEN_EDIT_MODAL:
+      return {
+        ...state,
+        isOpenModal: true,
+        editModalType: true,
+      };
+    case CLOSE_EDIT_MODAL:
+      return {
+        ...state,
+        isOpenModal: false,
+      };
     case USER_REQUESTED:
+    case USER_EDIT_REQUESTED:
+    case CHECK_USER_REQUESTED:
       return {
         ...state,
         isLoading: true,
         error: null,
       };
     case USER_REQUESTED_SUCCESSED:
+    case USER_EDIT_REQUESTED_SUCCESSED:
       return {
         ...state,
         isLoading: false,
         error: false,
         user: action.payload,
+        isOpenModal: false,
       };
     case USER_REQUESTED_FAILED:
+    case USER_EDIT_REQUESTED_FAILED:
+    case CHECK_USER_REQUESTED_FAILED:
       return {
         ...state,
         isLoading: false,
