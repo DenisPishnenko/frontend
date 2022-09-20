4874e73949f51f8610ace0dbfd6d3d82249bfdf2
d.pishnenko <d.pishnenko@dunice.net>
2022-09-20 17:01:40 +0300

    Add user checking and user avatar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,47 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 import { openModal, logout } from '../../redux/actions/auth';
+import { checkUser } from '../../redux/actions/user';
 
 import useStyles from './style';
 
 function Header() {
   const dispatch = useDispatch();
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const imageUrl = useSelector((state) => state.auth.user?.image.url);
+  const avatar = `${process.env.REACT_APP_API_URL}/${imageUrl}`;
   const classes = useStyles();
 
   const openAuthModal = (type) => dispatch(openModal(type));
   const logoutUser = () => dispatch(logout());
+
+  useEffect(() => {
+    if (isLoggedIn) dispatch(checkUser());
+  }, [isLoggedIn]);
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar className={classes.toolBar}>
           <Typography className={classes.title} variant="h6">
-            News App
+            <Link to="/" className={classes.link}>News App</Link>
           </Typography>
           {isLoggedIn ? (
-            <div>
+            <div className={classes.headerGroup}>
               <Button color="inherit" onClick={logoutUser}>
                 Logout
               </Button>
+              <div>
+                {avatar ? (<img src={avatar} alt="user" className={classes.userImage} />) : (<AccountCircleIcon />)}
+              </div>
             </div>
           ) : (
             <div>
